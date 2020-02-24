fd407dc8a69ff0fd93b2ea3c77c3f536b3fb8407
a.diachenko <alexdgx@gmail.com>
2020-02-24 20:58:56 +0200

    add hw1

---
+++ HomeWork1/proffesionalHW1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proffesionalHW1
+{
+    class Program
+    {
+        //With yield
+        public IEnumerable<int> SomeMethod (int [] array)
+        {
+            var result = array.Where(e => e % 2 != 0);
+            foreach (var item in result)
+            {
+                yield return item * item;
+            }
+        }
+        // Without 
+        public IEnumerable<int> SomeMethod2(int[] array)
+        {
+            return array.Where(e => e % 2 != 0).Select(e => e * e);
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
