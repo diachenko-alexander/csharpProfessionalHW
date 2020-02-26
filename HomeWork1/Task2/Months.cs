95932b9f0bd8adf8a173b2ec193fdd352544bac2
Alexander Diachenko <alexander.diachenko@creatio.com>
2020-02-26 17:32:46 +0200

    add Task2

--- HomeWork1/Task2/Months.cs
+++ HomeWork1/Task2/Months.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,29 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    class Months
+    class Months : IEnumerable, IEnumerator
     {
+        int[] month = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        int[] day = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+        public object Current ()
+        {
+
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
